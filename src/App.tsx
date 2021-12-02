e716c51b28d23de22fe3a20b3a33fcec3074bd2a
sudawan1114 <sudawan1114@protonmail.com>
2021-12-02 20:01:48 +0000

    update tesT

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+var engine = new Engine(canvas, true);
+          var scene = new Scene(engine);
+
+          var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+          camera.attachControl(canvas, true);
+          var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+          var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+
+          // hide/show the Inspector
+  //         window.addEventListener("keydown", (ev) => {
+  //                     // Shift+Ctrl+Alt+I
+  //                                 if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+  //                                                 if (scene.debugLayer.isVisible()) {
+  //                                                                     scene.debugLayer.hide();
+  //                                                                                     } else {
+  //                                                                                                         scene.debugLayer.show();
+  //                                                                                                                         }
+  //                                                                                                                                     }
+  //                                                                                                                                             });
+  //
+  //                                                                                                                                                     // run the main render loop
+  //                                                                                                                                                             engine.runRenderLoop(() => {
+  //                                                                                                                                                                         scene.render();
+  //                                                                                                                                                                                 });kkreturn (
+    <>
+<canvas id="gameCanvas"></canvas>
+    </>
   );
 }
 
